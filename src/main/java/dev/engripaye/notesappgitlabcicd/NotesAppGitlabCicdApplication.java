260c444856aaf15606edb08d5cfc3c8da7d9274e
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-18 02:01:29 +0100

    code commit By ENGR. IPAYE

---
+++ src/main/java/dev/engripaye/notesappgitlabcicd/NotesAppGitlabCicdApplication.java
@@ -0,0 +1,13 @@
+package dev.engripaye.notesappgitlabcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppGitlabCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAppGitlabCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/engripaye/notesappgitlabcicd/NotesAppGitlabCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engripaye.notesappgitlabcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppGitlabCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
